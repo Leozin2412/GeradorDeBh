fc4493fff41def635b87221a145d4bffbba3133b
Leonard Monteiro <leonardommonteiro2412@outlook.com>
2025-11-03 14:14:59 -0300

    .

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(cors({
 }));
 app.use((req, res, next) => {
     // Substitua pela sua origem frontend em produção:
-    const allowedOrigin=process.env.FRONTEND_URL ||"http://127.0.0.1:5500/index.html"
+    const allowedOrigin=process.env.FRONTEND_URL ||"http://127.0.0.1:5500"
     res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
     // Ou para qualquer origem durante o desenvolvimento (menos seguro):
     // res.setHeader('Access-Control-Allow-Origin', '*'); 
@@ -25,22 +25,4 @@ app.use((req, res, next) => {
 
 app.use(express.json());//Ativa body pars
 app.use(routes)
-/*
-app.listen(config.port,()=>{
-    console.log(`Servidor rodando em ${config.host}`)
-})*/
-
-
-
-
-
-
-/**CREATE TABLE TimeSheet (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    NTradsul VARCHAR(50) NOT NULL,
-    DtInicial DATETIME NOT NULL,
-    DtFinal DATETIME NOT NULL, -- Adicionei DtFinal aqui
-    Descricao TEXT,
-    TpIncidencia VARCHAR(50) NOT NULL,
-    Executante VARCHAR(50) NOT NULL
-); */
+ export default app
