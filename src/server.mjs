8923074802b2524a2ff162b874fa21c7bdcd1c21
Leonard Monteiro <leonardommonteiro2412@outlook.com>
2025-11-04 16:17:22 -0300

    .

--- src/server.mjs
+++ src/server.mjs
@@ -12,7 +12,7 @@ app.use(cors({
 }));
 app.use((req, res, next) => {
     // Substitua pela sua origem frontend em produção:
-    const allowedOrigin=process.env.FRONTEND_URL ||"http://127.0.0.1:5500/index.html"
+    const allowedOrigin=process.env.FRONTEND_URL ||"http://127.0.0.1:5500"
     res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
     // Ou para qualquer origem durante o desenvolvimento (menos seguro):
     // res.setHeader('Access-Control-Allow-Origin', '*'); 
