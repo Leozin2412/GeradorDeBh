2f7f40bc1b0559930951a9516ac716ab889a89ff
Leonard Monteiro <leonardommonteiro2412@outlook.com>
2025-11-04 15:06:36 -0300

    ,

--- src/repositories/TSrepositories.js
+++ src/repositories/TSrepositories.js
@@ -1,15 +1,15 @@
 
 
 
- import { PrismaClient } from '../../generated/prisma/index.js';
+/*  import { PrismaClient } from '../../generated/prisma/index.js';
 
 
-const prisma=new PrismaClient();
-/* import { PrismaClient } from '@prisma/client';
+const prisma=new PrismaClient(); */
+import { PrismaClient } from '@prisma/client';
 const prisma = global.prisma || new PrismaClient();
 if (process.env.NODE_ENV === 'development') {
     global.prisma = prisma;
-} */
+}
 
 const TSrepo={
     async importTS(seguradora,segurado,sinistroString,processo,DtInicial,DtFinal,descString,incidencia,executante){
